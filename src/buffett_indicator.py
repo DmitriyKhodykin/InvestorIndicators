b8d38c9850899ce6c0bd75f8c3f04e74db350c62
Dmitriy_Khodykin <d.hodikin@yandex.ru>
2021-08-19 16:22:23 +0300

    feat(project) Init commit

---
+++ src/buffett_indicator.py
@@ -0,0 +1,28 @@
+"""
+The Buffett Indicator is the ratio of total stock market valuation to GDP.
+"""
+import matplotlib.pyplot as plt
+import pandas
+
+
+def main():
+    pass
+
+
+class BuffettIndicator:
+
+    def __init__(self):
+        pass
+
+    def get_data(self):
+        pass
+
+    def indicator_calculation(self):
+        pass
+
+    def make_indicator_graph(self):
+        pass
+
+
+if __name__ == '__main__':
+    main()
